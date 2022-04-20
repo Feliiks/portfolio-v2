29833645b685395ad7629cb627d165082328c650
Feliiks <sobrero.ludovic@gmail.com>
2022-04-20 09:23:25 +0200

    Fix: responsive

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { HashLink } from 'react-router-hash-link'
 
@@ -6,32 +6,64 @@ import Logo from "../../assets/logo_allongé.png"
 
 
 const Index = () => {
+  const [navStatut, setNavStatut] = useState(false);
+  const navbarRef = useRef()
+
+  useEffect(() => {
+    if (navStatut) {
+      navbarRef.current.style.visibility = "visible";
+      navbarRef.current.style.opacity = "1";
+    } else {
+      navbarRef.current.style.visibility = "hidden";
+      navbarRef.current.style.opacity = "0";
+    }
+  })
+
+  useEffect(() => {
+    if (document.body.offsetWidth >= 768) {
+      setNavStatut(true)
+    }
+  })
+
+  useEffect(() => {
+    navbarRef.current.onclick = () => {
+      setNavStatut(false)
+    }
+  })
+
   return (
-    <div className="header container-fluid">
-      <div className="container">
-        <div className="logo">
-          <Link to="/">
-            <img src={Logo} alt="ls_logo" />
-          </Link>
+    <>
+      <div className="header container-fluid" ref={navbarRef}>
+        <div className="container">
+          <div className="logo">
+            <Link to="/">
+              <img src={Logo} alt="ls_logo" />
+            </Link>
+          </div>
+          <ul className="navbar-nav d-flex flex-md-row flex-column">
+            <li className="nav-item">
+              <Link className="nav-link" to="/about"> About </Link>
+            </li>
+            <li className="nav-item me-md-5 ms-md-5 mt-3 mb-3 mt-md-0 mb-md-0">
+              <Link className="nav-link" to="/works"> Works </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/pricing"> Pricing </Link>
+            </li>
+          </ul>
+          <HashLink smooth to="/#section-contact">
+            <button className="contact-btn btn btn-primary">
+              Contact me
+            </button>
+          </HashLink>
         </div>
-        <ul className="navbar-nav d-flex flex-row">
-          <li className="nav-item">
-            <Link className="nav-link" to="/about"> About </Link>
-          </li>
-          <li className="nav-item me-5 ms-5">
-            <Link className="nav-link" to="/works"> Works </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/pricing"> Pricing </Link>
-          </li>
-        </ul>
-        <HashLink smooth to="/#section-contact">
-          <button className="contact-btn btn btn-primary">
-            Contact me
-          </button>
-        </HashLink>
       </div>
-    </div>
+      <div className="burger" onClick={() => setNavStatut(!navStatut)}>
+        <div className="line-1" />
+        <div className="line-2" />
+        <div className="line-3" />
+      </div>
+    </>
   )
 }
 
